5917c0de7fdcfffc4b1d1d166fdcc5e46615417d
Matheus Piano <pianomatheus@gmail.com>
2019-10-15 18:50:52 -0300

    corrigindo pacman perdido

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Player : MonoBehaviour
 {
+    public float velocity = 1 * Time.deltaTime;
     CharacterController characterController;
     // Start is called before the first frame update
     void Start()
@@ -14,6 +16,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        characterController.Move(transform.forward);    
+        transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        characterController.Move(transform.forward * velocity);
     }
 }
